2a8459ee311276b5f03cce7496a239ead42d0395
pared <pawelredzynski@gmail.com>
2019-08-21 21:33:11 +0200

    data: use fastai to load data

--- src/data.py
+++ src/data.py
@@ -1,66 +1,25 @@
-import pandas
-
-import cv2, numpy as np
-import os
-
+from fastai.vision import *
 from src.utils import PROJECT_ROOT
 
+class DefectsSegList(SegmentationLabelList):
+    def open(self, id_rles):
+        image_id, rles = id_rles[0], id_rles[1:]
+        shape = open_image(self.path/image_id).shape[-2:]
+        final_mask = torch.zeros((1, *shape))
+        for k, rle in enumerate(rles):
+            if isinstance(rle, str):
+                mask = open_mask_rle(rle, shape).px.permute(0, 2, 1)
+                final_mask += (k+1)*mask
+        return ImageSegment(final_mask)
 
-class Data(object):
-    def __init__(self):
-        data_path = PROJECT_ROOT / "data" / "interim"
-        self.train_images_path = data_path / "train_images"
-
-        annotations_path = data_path / "train.csv"
-        self.annotations = pandas.read_csv(annotations_path)
-
-        self.image_names = os.listdir(self.train_images_path)
-
-    def load_image(self, image_name):
-        image_path = self.train_images_path / image_name
-        img = cv2.imread(str(image_path))
-        return img
-
-    def get_image_annotations(self, image_name):
-        """Returns map defect_id -> encoding for image name"""
-
-        image_rows = self.annotations[
-            self.annotations['ImageId_ClassId'].str.startswith(image_name)]
-
-        # NOTE there are 4 defects types, there MUST be 4 rows
-        assert len(image_rows) == 4
-
-        result = {}
-        for name, defect_encoding in image_rows.values:
-            key = int(name.split('_')[1])
-            if defect_encoding is np.nan:
-                defect_encoding = None
-            else:
-                defect_encoding = [int(e) for e in defect_encoding.split(" ")]
-            result[key] = defect_encoding
-        return result
-
-    def get_defects_masks(self, image_name):
-        image = self.load_image(image_name)
-        annotations = self.get_image_annotations(image_name)
-
-        result = {}
-
-        im_h, im_w, channels = image.shape
-        for defect_id, encoded_mask in annotations.items():
-            if encoded_mask:
-                bitmap_flat = np.zeros(im_w * im_h)
-                for i in range(0, len(encoded_mask), 2):
-                    start_index = encoded_mask[i]
-
-                    num_ones = encoded_mask[i + 1]
-
-                    for j in range(start_index, start_index + num_ones - 1):
-                        bitmap_flat[j] = 1
-                # NOTE ordering of pixels in encoding is top -> down, then left -> right, hence 'F'
-                bitmap = np.reshape(bitmap_flat, (im_h, im_w), order='F')
-                result[defect_id] = bitmap
-            else:
-                result[defect_id] = None
-
-        return result
+def load_data(bs=32, size=(256, 1600)):
+    labels_path = PROJECT_ROOT / "data" / "processed" / "train.csv"
+    images_path = PROJECT_ROOT / "data" / "interim" / "train_images"
+    train_list = (SegmentationItemList.
+                  from_csv(images_path, labels_path).
+                  split_by_rand_pct(valid_pct=0.2).
+                  label_from_df(cols=list(range(5)), label_cls=DefectsSegList, classes=[]).
+                  transform(size=size, tfm_y=True).
+                  databunch(bs=bs, num_workers=0).
+                  normalize(imagenet_stats))
+    return train_list
